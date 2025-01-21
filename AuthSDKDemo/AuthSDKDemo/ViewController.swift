7755f911773dba09b4d415bf0aba46a8337f23e7
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2025-01-20 19:13:16 -0500

    Create project

---
+++ AuthSDKDemo/AuthSDKDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthSDKDemo
+//
+//  Created by Mukhtar Yusuf on 1/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
